6fdb097e791810e2f3629b6dc859759bbd8cd60e
Victor Pudeyev <piousbox@gmail.com>
2012-09-30 10:47:34 -0700

    tests pass

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -14,23 +14,9 @@ class ReportsControllerTest < ActionController::TestCase
     setup_host
     
   end
+  
+  test 
 
-  test 'default values' do
-    
-    r = Report.where( :name => 'blah' ).each {|rr| rr.remove }
-    
-    # public, non-feature, non-trash
-    r = Report.new
-    r.name = 'blah'
-    r.save
-    
-    results = Report.where( :name => 'blah' )
-    assert_equal 1, results.length
-    result = results[0]
-    assert result.is_public
-    assert !result.is_feature
-    assert !result.is_trash
-    
-  end
+  
   
 end
