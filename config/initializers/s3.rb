aa133fc566dc1832d5ff4ed36760e45fc008f5ba
Victor Pudeyev <piousbox@gmail.com>
2012-10-18 19:32:10 -0700

    galleries tasks

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -14,7 +14,7 @@ else
   S3_CREDENTIALS = { 
     :access_key_id => "AKIAICS67L3FUORLSA7A", 
     :secret_access_key => "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", 
-    :bucket => "ISh-development"
+    :bucket => "ISh-trashy"
   }
 end
 
