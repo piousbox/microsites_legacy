625c225786ce43e618bc14b8580d0523bc8d88ee
piousbox <piousbox@gmail.com>
2012-12-13 12:00:22 -0800

    little

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,7 +11,19 @@ class Ability
     #
     unless user.blank?
 
+      can [ :create, :index, :new ], Addressbookitem
+      can :manage, Addressbookitem do |a|
+        a.user == user
+      end
+
+      can [ :create, :new, :index ], CitiesUser
+      can :manage, CitiesUser do |cu|
+        cu.user == user
+      end
+      
       can [ :set_city ], City
+
+      can [ :new, :create ], Community
 
       can [ :new, :create, :search, :index ], Day
       can :manage, Day do |day|
@@ -23,49 +35,41 @@ class Ability
         g.user == user
       end
 
-      can [ :upload, :create, :new, :driver ], Photo
+      can [ :upload, :create, :new, :driver, :set_profile_photo ], Photo
+      can [ :move, :edit, :update, :destroy ], Photo do |photo|
+        photo.user == user
+      end
       
       can [ :new, :create, :search, :index ], Report
       can [ :edit, :update ], Report do |r|
         r.user == user
       end
       
-      can [ :new, :create ], Tag
+      can [ :new, :create, :index ], Tag
+      can [ :update, :edit, :destroy ], Tag do |tag|
+        tag.user == user
+      end
 
       can [ :organizer, :account, :photos, :scratchpad ], User
 
-      #      can [ :create, :index, :new ], Addressbookitem
-      #      can :manage, Addressbookitem do |a|
-      #        a.user_id == user.id
-      #      end
-      #
-      #      can [ :create, :new ], CitiesUser
-      #      can :manage, CitiesUser do |cu|
-      #        cu.user_id == user.id
-      #      end
-      #      
-      #      can [ :set_profile_photo, :move, :driver, :create_for_gallery, :create ], Photo
-      #      can [ :edit, :update ], Photo do |ph|
-      #        ph[:user_id] == user[:id]
-      #      end
-      #      
+      can [ :manage ], Venue do |venue|
+        venue.user == user
+      end
+
+      can [ :manage ], Video do |video|
+        video.user == user
+      end
 
       #
       # manager
       #
       if user[:group_id] <= 2
 
+        can [ :manage ], City
+        can [ :manage ], Gallery
         can [ :manage ], Report
-
-        can [ :manager ], Tag
-
-        #      can [ :mark_features ], Gallery
-        #
-        #      can [ :mark_features, :manager_dashboard ], Report
-        #
-        #      can [ :mark_features ], Video
-        #
-        #      can [ :manage ], City
+        can [ :manage ], Tag
+        can [ :manage ], Video
 
       end
 
@@ -90,7 +94,7 @@ class Ability
     
     can [ :profile, :index, :show, :map, :events, :galleries, :videos, :reports, :people, :users, :venues, :places ], City
     
-    can [ :index ], Gallery
+    can [ :index, :search ], Gallery
     can [ :show ], Gallery do |g|
       g.is_public && !g.is_trash
     end
@@ -108,7 +112,7 @@ class Ability
 
     can [ :new, :create, :success ], Subscription
     
-    can [ :index ], Tag
+    can [ :index, :search ], Tag
     can [ :show ], Tag do |t|
       t.is_public && !t.is_trash
     end
@@ -129,64 +133,16 @@ class Ability
 
     can [ :set_locale, :home ], Welcome
     
-    #    
-    #    can [ :index, :index_small, :new, :create, :show, :events, :users, :join ], Community
-    #    can :manage, Community do |community|
-    #      community.user_id == user.id
-    #    end
-    #
-    #    can [ :free_photo_hosting, :index_small, :search, :index_2 ], Gallery 
-    #    can [ :show ], Gallery do |g|
-    #      g[:is_public] == 1 && g[:is_trash] == 0
-    #    end
-    #
-    #    can [:index, :index_small, :new, :create, :show], Event
-    #    can [ :show], Event do |ee|
-    #      ee.is_public && !ee[:is_trash]
-    #    end
-    #    can :manage, Event do |e|
-    #      e.user_id.to_s == user.id.to_s
-    #    end
-    #    
-    #    can [ :index, :homepage, :global_feature, :index_small, :popup,
-    #      :search_google, :search, :html_data, :search, :old_content ], Report
-    #    can [ :show, :promo ], Report do |r|
-    #      r[:is_public] == 1 && r[:is_trash] == 0
-    #    end
-    #    can :manage, Report do |r|
-    #      r[:user_id] == user[:id]
-    #    end
-    #
-    #    can [ :index, :index_small, :reports, :homepage ], Tag
-    #    can [ :show], Tag do |t|
-    #      t.is_public == 1 && t[:is_trash] == 0
-    #    end
-    #    can :manage, Tag do |tag|
-    #      tag.user_id == user.id
-    #    end
-    #    
-    #    can [ :show, :index, :index_small, :new, :html_data, 
-    #      :reports_data, :galleries_data, :post_layout, :set_username,
-    #      :sign_in, :sign_out, :facebook, :failure ], User
-    #
-    #    can [ :index, :index_small ], Venue
-    #    can :manage, Venue do
-    #      user[:group_id] == 2
-    #    end
-    #    can [ :show ], Venue do |v|
-    #      v[:is_public] && !v[:is_trash]
-    #    end
-    #    can :manage, Venue do |v|
-    #      v.user_id == user.id
-    #    end
-    #
-    #    can [ :index ], Video
-    #    can [ :show ], Video do |v|
-    #      v[:is_public] && !v[:is_trash]
-    #    end
-    #    can :manage, Video do |v|
-    #      v[:user_id] == user[:id]
-    #    end
+    can [ :index ], Venue
+    can [ :show ], Venue do |v|
+      v.is_public && !v.is_trash
+    end
+    
+    can [ :index ], Video
+    can [ :show ], Video do |v|
+      v.is_public && !v.is_trash
+    end
+        
     
   end
 end
