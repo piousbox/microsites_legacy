f9ca2c23bdadf5fe8f1db94c475919ecd423c376
piousbox <piousbox@gmail.com>
2013-01-02 11:40:25 -0800

    layout of users/sign_in

--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -14,14 +14,21 @@ describe Users::SessionsController do
     @request.env["devise.mapping"] = Devise.mappings[:user]
   end
 
+
   describe 'sign in' do
     it 'should render proper layout' do
-      [ 'organizer.local', 'organizer.annesque.com', 'qxt.local', 'pi.local', 'piousbox.com' ].each do |h|
+      layouts = [ 'layouts/organizer', 'layouts/organizer', 'layouts/organizer',
+        'layouts/organizer', 'layouts/organizer',
+        'layouts/cac', 'layouts/cac'
+      ]
+      [ 'organizer.local', 'organizer.annesque.com', 'qxt.local',
+        'pi.local', 'piousbox.com',
+        'cac.local', 'computationalartscorp.com' ].each_with_index do |h, idx|
         @request.host = h
         get :new
         # puts! @response.body
         response.should be_success
-        response.should render_template('layouts/organizer')
+        response.should render_template(layouts[idx])
       end
     end
   end
