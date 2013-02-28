a350df220aefd18ea56e805ceed7ad76029de467
piousbox <piousbox@gmail.com>
2013-02-27 17:15:47 -0800

    assets are compiled over on the node itself, not locally

--- app/controllers/manager/nodes_controller.rb
+++ app/controllers/manager/nodes_controller.rb
@@ -5,11 +5,11 @@ class Manager::NodesController < Manager::ManagerController
 
   def run_client
 
-    @result_1 = ''
+    # @result_1 = ''
     # @result_1 = @result_1 + `cd ~/projects/microsites2 && be rspec spec`
     # @result_1 = @result_1 + `cd ~/projects/microsites2 && be rake test`
-    @result_1 = @result_1 + `cd ~/projects/microsites2 && be rake assets:precompile && git add . && git commit -am "automated commit" && git push origin master`
-    @result_1 = @result_1 + `cd ~/projects/microsites2 && rm -rf public/assets`
+    # @result_1 = @result_1 + `cd ~/projects/microsites2 && be rake assets:precompile && git add . && git commit -am "automated commit" && git push origin master`
+    # @result_1 = @result_1 + `cd ~/projects/microsites2 && rm -rf public/assets`
 
     node = @nodes.select { |n| n[:node_name] == params[:node_name] }[0]
     Net::SSH.start( @host, @user, :port => node[:port], :keys => @keys ) do|ssh|
@@ -18,17 +18,6 @@ class Manager::NodesController < Manager::ManagerController
       @result = @result + ssh.exec!('sudo service microsites2-app restart')
     end
     
-  end
-
-  def restart_service
-
-    node = @nodes.select { |n| n[:node_name] == params[:node_name] }[0]
-    Net::SSH.start( @host, @user, :port => node[:port], :keys => @keys ) do|ssh|
-      @result = ''
-      @result = @result + ssh.exec!('sudo chef-client')
-      @result = @result + ssh.exec!('sudo service microsites2-app restart')
-    end
-
   end
 
   private
