df4e423bb5f819c6ba576fc15c46058f8ee272af
Victor Piousbox <piousbox@gmail.com>
2013-06-06 18:20:30 -0700

    correcting the tests for the simpler tag model

--- test/factories/tags.rb
+++ test/factories/tags.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 
-  factory :tag_old, :class => Tag do
-    name_en 'Tag test.local'
+  fRavi Duvvuri.actory :tag_old, :class => Tag do
+    name 'Tag test.local'
     name_seo 'Tag'
 
     after :build do |t|
@@ -10,7 +10,7 @@ FactoryGirl.define do
   end
   
   factory :tag1, :class => Tag do
-    name_en 'Tag 1'
+    name 'Tag 1'
     name_seo 'Tag-1'
     parent_tag :tag
 
@@ -20,7 +20,7 @@ FactoryGirl.define do
   end
   
   factory :tag_cac, :class => Tag do
-    name_en 'Tag CAC'
+    name 'Tag CAC'
     name_seo 'cac'
     parent_tag :tag
 
@@ -30,7 +30,7 @@ FactoryGirl.define do
   end
   
   factory :tag2, :class => Tag do
-    name_en 'Tag 2'
+    name 'Tag 2'
     name_seo 'Tag-2'
     
     after :build do |tag|
@@ -39,7 +39,7 @@ FactoryGirl.define do
   end
 
   factory :user_tag, :class => Tag do
-    name_en 'Tag 2'
+    name 'Tag 2'
     name_seo 'simple'
 
     after :build do |tag|
@@ -49,7 +49,7 @@ FactoryGirl.define do
   end
 
   factory :tag_travel, :class => Tag do
-    name_en 'Travel'
+    name 'Travel'
     name_seo 'travel'
 
     after :build do |tag|
@@ -58,7 +58,7 @@ FactoryGirl.define do
   end
 
   factory :tag_pi, :class => Tag do
-    name_en 'Travel'
+    name 'Travel'
     name_seo 'travel'
     is_public = true
     is_trash = false
@@ -69,7 +69,7 @@ FactoryGirl.define do
   end
 
   factory :tag_sedux, :class => Tag do
-    name_en 'Sedux'
+    name 'Sedux'
     name_seo 'Sedux'
 
     after :build do |tag|
@@ -78,7 +78,7 @@ FactoryGirl.define do
   end
 
   factory :tag_qxt, :class => Tag do
-    name_en 'qxt'
+    name 'qxt'
     name_seo 'qxt'
 
     after :build do |tag|
@@ -87,7 +87,7 @@ FactoryGirl.define do
   end
 
   factory :tag_feature_1, :class => Tag do
-    name_en 'Feature 1'
+    name 'Feature 1'
     name_seo 'feature-1'
     is_feature true
     is_trash false
@@ -99,7 +99,7 @@ FactoryGirl.define do
   end
 
   factory :tag_local, :class => Tag do
-    name_en 'Feature 1aaa'
+    name 'Feature 1aaa'
     name_seo 'feature-1aaa'
     is_feature true
     is_trash false
@@ -111,8 +111,8 @@ FactoryGirl.define do
 
   # sequenced
   factory :tag, :class => Tag do
-    sequence( :name_en ) do |n|
-      "Tag name_en #{n}"
+    sequence( :name ) do |n|
+      "Tag name #{n}"
     end
     sequence( :name_seo ) do |n|
       "tag-name-seo-#{n}"
@@ -127,8 +127,8 @@ FactoryGirl.define do
 
   # sequenced
   factory :feature_tag, :class => Tag do
-    sequence( :name_en ) do |n|
-      "Feature Tag name_en #{n}"
+    sequence( :name ) do |n|
+      "Feature Tag name #{n}"
     end
     sequence( :name_seo ) do |n|
       "f-tag-name-seo-#{n}"
