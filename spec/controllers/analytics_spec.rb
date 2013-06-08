621199c39a07a82d56ddb89b97b45e38da7a9029
Victor Piousbox <piousbox@gmail.com>
2013-06-07 17:15:47 -0700

    analytics diversification complete

--- spec/controllers/analytics_spec.rb
+++ spec/controllers/analytics_spec.rb
@@ -1,15 +1,29 @@
 require 'spec_helper'
 describe SitesController do
   render_views
+
   before :each do
-    setup_sites
+    Site.all.each { |s| s.remove }
+    @hosts_and_ids = [
+                      [ 'piousbox.com', '#analytics_pi' ],
+                      [ 'webdevzine.com', '#analytics_wdz' ],
+                      [ 'sedux.net', '#analytics_sedux' ],
+                      [ 'computationalartscorp.com', '#analytics_cac' ],
+                      [ 'travel-guide.mobi', '#analytics_mobi' ],
+                      [ 'annesque.com', '#analytics_pi' ]
+                     ]
+    @hosts_and_ids.each do |i|
+      Site.create :domain => i[0], :lang => :en 
+    end
   end
 
   describe "analytics group" do
     it 'should render the piousbox.com analytics' do
-      @request.host = 'piousbox.com'
-      get :show, :domain => 'piousbox.com'
-      assert_select( "#analytics_pi" )
+      @hosts_and_ids.each do |item|
+        @request.host = item[0]
+        get :show, :domain => item[0]
+        assert_select( item[1] )
+      end
     end
   end
 
