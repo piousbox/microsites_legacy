c20aeb34f593b432a29014b6ad26d366af21e2d3
Victor Piousbox <piousbox@gmail.com>
2013-06-12 12:52:20 -0700

    manager should be able to login

--- app/controllers/manager/manager_controller.rb
+++ app/controllers/manager/manager_controller.rb
@@ -11,13 +11,14 @@ class Manager::ManagerController < ApplicationController
   protected
   
   def require_manager
-    return true if Rails.env.test?
-    authenticate_or_request_with_http_basic do |username, password|
-      if @current_user.blank?
-        redirect_to new_user_session_path
-      else
+    if Rails.env.test?
+      return
+    elsif @current_user.blank?
+      redirect_to new_user_session_path
+    else
+      authenticate_or_request_with_http_basic do |username, password|
         username == 'piousbox' && password == 'sho3b0x3' && ( @current_user.username == 'piousbox' || @current_user.username == 'manager' )
-      end
+      end       
     end
   end
 
