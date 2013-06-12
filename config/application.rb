64a360105346aa3f067bc920ad9f99f526675153
Victor Piousbox <piousbox@gmail.com>
2013-06-12 11:59:39 -0700

    precompiling assets rails.js

--- config/application.rb
+++ config/application.rb
@@ -49,7 +49,7 @@ module Microsites2
 
     config.assets.paths << Rails.root.join("app", "assets", "flash")
     config.assets.precompile += [ 'cities.css', 'manager.css', 'organizer.css', 'print.css', 'resume.css', 'm.css', 'spec_runner.css' ]
-    config.assets.precompile += [ 'm.js', 'spec_runner.js', 'manager.js' ]
+    config.assets.precompile += [ 'm.js', 'spec_runner.js', 'manager.js', 'rails.js' ]
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
