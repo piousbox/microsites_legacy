d8f646288ae76a23f8a7b7a78e4c940698f7a1e5
Victor Piousbox <piousbox@gmail.com>
2013-07-16 20:09:39 -0700

    whatever

---
+++ bin/cron-jobs.sh
@@ -0,0 +1,1 @@
+* * * * * PATH=$PATH:/usr/local/rbenv/shims /home/ubuntu/bin/do >> /home/ubuntu/trash/outcome 2>&1
--- bin/push-branch.sh
+++ bin/push-branch.sh
@@ -2,14 +2,19 @@
 
 branch_name=$1
 
+echo "adding, committing and pushing all to $branch_name"
 git add .
 git commit -am "automated push of $branch_name"
 git push origin $branch_name
+
+echo "mergine to master, compiling and pushing"
 git checkout master
 git merge $branch_name
-bundle exec rake assets:precompile
+bundle exec rake assets:precompile --trace
 git add .
 git commit -am "automated push of branch $branch_name"
 git push origin master
+
+echo "switching back to branch $branch_name"
 git checkout branch_name
 git status
