53dee5263a8b84a2ca517eb22c5941871064a564
Victor Piousbox <piousbox@gmail.com>
2013-09-12 11:03:00 -0700

    resolved a spec error for tags

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -14,11 +14,9 @@ describe TagsController do
 
     Tag.all.each { |t| t.remove }
     @tag = FactoryGirl.create :tag_technology
-    @r.tag = @tag && @r.save
-    @r1.tag = @tag && @r1.save
-    @g.tag = @tag && @g.save
-    @g1.tag = @tag && @g1.save
-
+    @tag.reports << [ @r, @r1 ]
+    @tag.galleries << [ @g, @g1 ]
+    @tag.save
   end
 
   it 'GETs index' do
@@ -28,20 +26,21 @@ describe TagsController do
   end
 
   it 'GETs show' do
-    tag = Tag.where( :name_seo => 'technology' ).first
-    tag.should_not eql nil
+    @tag.should_not eql nil
+    @tag.reports.length.should > 1
+    @tag.galleries.length.should > 1
 
-    get :show, :tagname => 'technology'
+    get :show, :tagname => @tag.name_seo
     response.should render_template( 'tags/show' )
     assigns( :tag ).should_not eql nil
 
     # correct ordering of reports
     assigns( :reports ).length.should >= 2
-    assigns( :reports )[0].created_at.should > assigns( :reports )[1].created_at
+    assigns( :reports )[0].created_at.should >= assigns( :reports )[1].created_at
 
     # correct ordering of galleries
     assigns( :galleries ).length.should >= 2
-    assigns( :galleries )[0].created_at.should > assigns( :galleries )[1].created_at
+    assigns( :galleries )[0].created_at.should >= assigns( :galleries )[1].created_at
   end
 
 end
