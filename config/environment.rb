76d0c3c48a6515546fc34188170691f8c17bf5fa
Victor Pudeyev <vpudeyev@itu.edu>
2013-09-25 15:19:36 -0700

    encoding

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,11 @@ require File.expand_path('../application', __FILE__)
 Microsites2::Application.initialize!
 
 require 'paperclip'
+
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
+# encoding: utf-8
+# coding: utf-8
+
+YAML::ENGINE.yamler = 'syck'
