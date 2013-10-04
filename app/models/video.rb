53761e915ad64d8d8137a26e8be9186a37bb482e
Victor Pudeyev <vpudeyev@itu.edu>
2013-10-04 11:30:58 -0700

    validation of video

--- app/models/video.rb
+++ app/models/video.rb
@@ -24,7 +24,7 @@ class Video
   field :lang, :type => String, :default => 'en'
 
   field :youtube_id, :type => String
-  validates :youtube_id, :uniqueness => true, :presence => truexs
+  validates :youtube_id, :uniqueness => true, :presence => true
 
   belongs_to :tag
   belongs_to :city
