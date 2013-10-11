f68ed9fd62f04fdf619b9d5eab313dc60cfd2f08
Victor Piousbox <piousbox@gmail.com>
2013-10-11 15:57:17 -0700

    fixed routing spec. fixed all specs

--- spec/routing/users_spec.rb
+++ spec/routing/users_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 describe 'Users' do
 
   it 'print resume' do
-    expect( :get => '/en/users/pi/resume/print' ).to route_to( 'users#', :locale => 'en', :print => true, :username => 'pi', :action => 'show' )
+    expect( :get => '/en/users/pi/resume/print' ).to route_to( 'users#show', :locale => 'en', :print => true, :username => 'pi' )
   end
 
 end
