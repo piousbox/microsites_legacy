6c5a5917d5f6e4d2bf4ec32f0e8e1d45ad876c17
Victor Piousbox <piousbox@gmail.com>
2013-10-21 19:20:14 -0700

    removing garbage email notifiers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,7 @@ class UserMailer < ActionMailer::Base
   default :from => "admin@piousbox.com"
 
   def welcome_email(user)
+    @site = Site.where( :domain => request.domain, :lang => 'en' ).first || Site.new
     @user = user
     @url = 'http://piousbox.com/en/users/sign_in'
     mail( :to => user.email, :subject => 'Welcome to Piousbox.com' )
