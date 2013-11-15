012cc50d34b8051e5ad5c90f3c359cf4b24c4ed7
Victor Piousbox <piousbox@gmail.com>
2013-11-15 10:21:52 -0800

    I need to re-pass all unit tests

--- lib/string.rb
+++ lib/string.rb
@@ -2,8 +2,8 @@
 class String
   
   def to_simple_string
-    a = self.gsub("\.", '-')
-    a = a.gsub(':', '-').gsub('%20', '-')
+    a = self.gsub('/', '').gsub('\.', '').gsub('(','').gsub(')','')
+    a = a.gsub(':', '').gsub('%20', '-')
     a = a.downcase.strip.gsub(' ', '-')
     while a.include? '--'
       a = a.sub("--", '-')
--- test/unit/lib/string_test.rb
+++ test/unit/lib/string_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class StringTest < ActiveSupport::TestCase
   
   test 'to_simple_string' do
-    string = 'Blah.blah'
-    assert_equal 'blah-blah', string.to_simple_string
-    
+    assert_equal 'blah-blah', 'Blah.blah'.to_simple_string
+    assert_equal 'blah-blah', 'blah/blah'.to_simple_string
+    assert_equal 'blah-blah', 'blah\blah'.to_simple_string
   end
-  
+
 end
